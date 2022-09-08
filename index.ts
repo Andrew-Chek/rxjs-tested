89af5bd204adf4cf2d3eb636e10c14acb13c411d
Andrew-Chek <69400461+Andrew-Chek@users.noreply.github.com>
2022-09-08 21:55:41 +0300

    cold interval here we go again

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import './style.css';
 
-import { Observable, catchError, of, from, interval, filter } from 'rxjs';
+import { Observable, catchError, of, from, interval, take } from 'rxjs';
 import { ajax } from 'rxjs/ajax';
 
 // Створити Observable, яка буде віддавати 2 синхронні значення "1", "2", а через 2 секунди викидувати помилку. Ваша задача використовуючи існуючі оператори обробити цю помилку всередині pipe, для того, щоб вона не дійшла до subscribe
@@ -42,14 +42,8 @@ const eventObservable = formEvent(document, 'click').subscribe({
 setTimeout(() => eventObservable.unsubscribe(), 5000)*/
 
 // Використовуючи оператор interval, підписатися на нього і слухати до того моменту, доки значення не буде більше 5(використовуючи оператор в pipe)
-
-const num = from([5])
-
-function filterNum() {
-  return filter((value) => value < num);
-}
-  const intervalObservable = interval(500).pipe(filterNum());
-  intervalObservable.subscribe((value) => console.log(value))
+  // const intervalObservable = interval(500).pipe(take(5));
+  // intervalObservable.subscribe((value) => console.log(value))
 
 // Перетворіть coldInterval нижче на hotInterval, щоб він став гарячим(віддавав одні і ті ж значення різним підписникам)
 // Приклад:
@@ -62,7 +56,7 @@ function filterNum() {
 // sub1 3
 // sub2 3
 
-/*function coldInterval() {
+function coldInterval() {
   return new Observable((subscriber) => {
     let count = 0;
     const intervalId = setInterval(() => {
@@ -99,17 +93,17 @@ function subscribe()
 }
 setTimeout(subscribe, 3000);
 
-// Обробити відповідь запиту, в pipe спочатку витягнути об'єкт response(це масив), відфільтруєте масив так, щоб залишилися тільки пости з id менше 5.
-// Hint: так як response - це буде масив постів, ви не можете просто фідфільтрувати його через filter(він приймає кожен елемент масиву, а не цілий масив). Для рішення цієї задачі вам потрібні оператори mergeMap або concatMap, в яких ви зробите з(перекладіть англійською) масиву потік окремих елементів масиву([1, 2, 3] => 1, 2, 3), відфільтруєте їх,а потім зберете назад в масив за допомогою оператора. В subscribe ми отримаємо масив з 4 об'єктів id яких менше 5
+// // Обробити відповідь запиту, в pipe спочатку витягнути об'єкт response(це масив), відфільтруєте масив так, щоб залишилися тільки пости з id менше 5.
+// // Hint: так як response - це буде масив постів, ви не можете просто фідфільтрувати його через filter(він приймає кожен елемент масиву, а не цілий масив). Для рішення цієї задачі вам потрібні оператори mergeMap або concatMap, в яких ви зробите з(перекладіть англійською) масиву потік окремих елементів масиву([1, 2, 3] => 1, 2, 3), відфільтруєте їх,а потім зберете назад в масив за допомогою оператора. В subscribe ми отримаємо масив з 4 об'єктів id яких менше 5
 
-ajax('https://jsonplaceholder.typicode.com/posts');
+// ajax('https://jsonplaceholder.typicode.com/posts');
 
-// Використовуючи Rxjs написати потік, який буде слухати кліки по кнопці і відправляти при натисканню на неї запит на сервер із текстом введеним в пошук. В subscribe ми маємо отримати дані з серверу.
-// Оператори, які можуть знадобитися: fromEvent, switchMap, ajax, map, etc
+// // Використовуючи Rxjs написати потік, який буде слухати кліки по кнопці і відправляти при натисканню на неї запит на сервер із текстом введеним в пошук. В subscribe ми маємо отримати дані з серверу.
+// // Оператори, які можуть знадобитися: fromEvent, switchMap, ajax, map, etc
 
-const search = document.querySelector('input');
-const button = document.querySelector('button');
+// const search = document.querySelector('input');
+// const button = document.querySelector('button');
 
-// Використовуючи RxJs зробити свою імплементацію Drag&Drop.
-// Деталі: Створіть 3 observable mousedown$, mousemove$, mouseup$. Які будуть слухати події mousedown, mousemove, mouseup відповідно. Ваша задача поєднати їх так, щоб mousemove$ починав працювати тільки коли користувач натикає  на mousedown, і переставали слухати, коли відбувається mouseup. Тобто постійно ви маєте слухати тільки mousedown, а підписуватися на зміну mousemove i mouseup тільки після івенту mousedown
-// const mousedown$ = ... .pipe().subscribe(value - колекція mousemove подій, яка починається віддаватися при mousedown і закінчує стрім при mouseup)*/
+// // Використовуючи RxJs зробити свою імплементацію Drag&Drop.
+// // Деталі: Створіть 3 observable mousedown$, mousemove$, mouseup$. Які будуть слухати події mousedown, mousemove, mouseup відповідно. Ваша задача поєднати їх так, щоб mousemove$ починав працювати тільки коли користувач натикає  на mousedown, і переставали слухати, коли відбувається mouseup. Тобто постійно ви маєте слухати тільки mousedown, а підписуватися на зміну mousemove i mouseup тільки після івенту mousedown
+// // const mousedown$ = ... .pipe().subscribe(value - колекція mousemove подій, яка починається віддаватися при mousedown і закінчує стрім при mouseup)
